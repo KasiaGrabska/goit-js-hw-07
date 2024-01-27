20c293a8201f61063065cf8685ba6fc641f58025
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-01-27 11:47:20 +0100

    Gallery is ready :D

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+//zmienna za pomocą, której dostajemy się do galeri
+const gallery = document.querySelector(".gallery");
+
+const listItems = galleryItems
+  .map(
+    (galleryItem) =>
+      `<li class="gallery__item">
+            <a class="gallery__link" href="${galleryItem.original}">
+                <img
+            class="gallery__image"
+            src="${galleryItem.preview}"
+            data-source="${galleryItem.original}"
+            alt="${galleryItem.description}"
+            >
+            </img>
+            </a>
+    </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", listItems);
 
 console.log(galleryItems);
