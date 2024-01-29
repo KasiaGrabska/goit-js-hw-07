d5f3217b8de267091ad647c323499e13127343f7
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-01-29 17:57:37 +0100

    Change second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,9 +3,10 @@ import { galleryItems } from "./gallery-items.js";
 //zmienna za pomocą, której dostajemy się do galeri
 const gallery = document.querySelector(".gallery");
 
-const listItems = galleryItems.map(
-  (galleryItem) =>
-    `<li class="gallery__item">
+const listItems = galleryItems
+  .map(
+    (galleryItem) =>
+      `<li class="gallery__item">
             <a class="gallery__link" href="${galleryItem.original}">
                 <img
             class="gallery__image"
@@ -14,9 +15,10 @@ const listItems = galleryItems.map(
               />
             </a>
     </li>`
-);
+  )
+  .join("");
 
-gallery.innerHTML = listItems.join("");
+gallery.insertAdjacentHTML("afterbegin", listItems);
 
 const lightbox = new SimpleLightbox(".gallery a", {
   captions: true,
